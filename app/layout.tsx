566df955d93c2a5226fdbae24a875ce84a4ef30d
SoUrAv <aansourav@gmail.com>
2023-12-19 21:42:38 +0600

    OutCourse

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import { ConfettiProvider } from '@/components/providers/confetti-provider'
+import { ToastProvider } from '@/components/providers/toaster-provider'
+import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'OutCourse',
+  description: 'Made with ♥  by Abdullah An-Noor',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ConfettiProvider />
+          <ToastProvider />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
